6c5f807f8456a80cb6de489980302e48b3c4c9ba
kayyraa <zindhukayrabasbay@gmail.com>
2025-01-05 16:50:12 +0300

    Auto-update from local project folder

---
+++ scripts/rom.js
@@ -0,0 +1,185 @@
+export const Elements = [
+	{
+		Name: "Hydrogen",
+		Symbol: "H",
+		Radius: 0.25,
+		Color: "rgb(171, 171, 171)"
+	},
+	{
+		Name: "Helium",
+		Symbol: "He",
+		Radius: 0.3,
+		Color: "rgb(217, 255, 199)"
+	},
+	{
+		Name: "Lithium",
+		Symbol: "Li",
+		Radius: 0.6,
+		Color: "rgb(203, 128, 240)"
+	},
+	{
+		Name: "Beryllium",
+		Symbol: "Be",
+		Radius: 0.35,
+		Color: "rgb(194, 255, 199)"
+	},
+	{
+		Name: "Boron",
+		Symbol: "B",
+		Radius: 0.37,
+		Color: "rgb(255, 181, 181)"
+	},
+	{
+		Name: "Carbon",
+		Symbol: "C",
+		Radius: 0.3,
+		Color: "rgb(70, 70, 70)"
+	},
+	{
+		Name: "Nitrogen",
+		Symbol: "N",
+		Radius: 0.32,
+		Color: "rgb(34, 147, 178)"
+	},
+	{
+		Name: "Oxygen",
+		Symbol: "O",
+		Radius: 0.35,
+		Color: "rgb(255, 47, 0)"
+	},
+	{
+		Name: "Fluorine",
+		Symbol: "F",
+		Radius: 0.28,
+		Color: "rgb(115, 80, 255)"
+	},
+	{
+		Name: "Neon",
+		Symbol: "Ne",
+		Radius: 0.27,
+		Color: "rgb(133, 67, 255)"
+	},
+	{
+		Name: "Sodium",
+		Symbol: "Na",
+		Radius: 0.5,
+		Color: "rgb(196, 145, 117)"
+	},
+	{
+		Name: "Magnesium",
+		Symbol: "Mg",
+		Radius: 0.45,
+		Color: "rgb(138, 255, 199)"
+	},
+	{
+		Name: "Aluminum",
+		Symbol: "Al",
+		Radius: 0.43,
+		Color: "rgb(191, 166, 166)"
+	},
+	{
+		Name: "Silicon",
+		Symbol: "Si",
+		Radius: 0.4,
+		Color: "rgb(240, 200, 140)"
+	},
+	{
+		Name: "Phosphorus",
+		Symbol: "P",
+		Radius: 0.38,
+		Color: "rgb(255, 128, 0)"
+	},
+	{
+		Name: "Sulfur",
+		Symbol: "S",
+		Radius: 0.4,
+		Color: "rgb(244, 164, 96)"
+	},
+	{
+		Name: "Chlorine",
+		Symbol: "Cl",
+		Radius: 0.3,
+		Color: "rgb(0, 255, 0)"
+	},
+	{
+		Name: "Argon",
+		Symbol: "Ar",
+		Radius: 0.34,
+		Color: "rgb(199, 127, 255)"
+	},
+	{
+		Name: "Potassium",
+		Symbol: "K",
+		Radius: 0.7,
+		Color: "rgb(143, 64, 212)"
+	},
+	{
+		Name: "Calcium",
+		Symbol: "Ca",
+		Radius: 0.65,
+		Color: "rgb(61, 255, 199)"
+	},
+	// Additional Elements
+	{
+		Name: "Scandium",
+		Symbol: "Sc",
+		Radius: 0.75,
+		Color: "rgb(153, 153, 255)"
+	},
+	{
+		Name: "Titanium",
+		Symbol: "Ti",
+		Radius: 0.8,
+		Color: "rgb(182, 182, 255)"
+	},
+	{
+		Name: "Vanadium",
+		Symbol: "V",
+		Radius: 0.85,
+		Color: "rgb(255, 183, 255)"
+	},
+	{
+		Name: "Chromium",
+		Symbol: "Cr",
+		Radius: 0.9,
+		Color: "rgb(169, 169, 169)"
+	},
+	{
+		Name: "Manganese",
+		Symbol: "Mn",
+		Radius: 0.95,
+		Color: "rgb(128, 128, 255)"
+	},
+	{
+		Name: "Iron",
+		Symbol: "Fe",
+		Radius: 1.0,
+		Color: "rgb(200, 0, 0)"
+	},
+	{
+		Name: "Cobalt",
+		Symbol: "Co",
+		Radius: 1.1,
+		Color: "rgb(0, 0, 255)"
+	},
+	{
+		Name: "Nickel",
+		Symbol: "Ni",
+		Radius: 1.2,
+		Color: "rgb(0, 255, 0)"
+	},
+	{
+		Name: "Copper",
+		Symbol: "Cu",
+		Radius: 1.3,
+		Color: "rgb(255, 165, 0)"
+	},
+	{
+		Name: "Zinc",
+		Symbol: "Zn",
+		Radius: 1.4,
+		Color: "rgb(128, 128, 0)"
+	}
+];
+
+export const RadiusConstant = Math.pow(2, 7);
